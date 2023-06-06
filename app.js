4aacdde35d2c28dedc6dc049ebf01fe3faa2073d
Sazzad Bashar Rakib <sazzad__bashar@outlook.com>
2023-06-06 23:36:40 +0600

    updating second commit

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ anchorElement.href = 'https://google.com';
 
 anchorElement = document.querySelector('p a'); // p a { color: red; }
 anchorElement.href = 'https://academind.com';
+
+anchorElement = document.querySelector;
